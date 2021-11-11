01ba9a0a2b19f1a42354fdbf07c8361f413aeea6
rabida <rabeb.abida@unamur.be>
2021-11-11 16:28:38 +0100

    temporary fix listwikilink

--- application/PageTwo.py
+++ application/PageTwo.py
@@ -757,7 +757,7 @@ class PageTwo(Frame):
             tvI.insert("", "end",values=row)
 
         #Show button selection
-        button_Q2_SelectProposition = tk.Button(self.frame_questions, text='Add', command=lambda:self.algo_question_proposition(tvI))
+        button_Q2_SelectProposition = tk.Button(self.frame_questions, text='Add', command=lambda:self.algo_question_proposition(tvI, None))
         button_Q2_SelectProposition.pack()
 
 
